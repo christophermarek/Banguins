aa5dfb6273d30b450bc3a6a41962d4f764fa6992
christophermarek <christophermarek42@gmail.com>
2022-03-04 19:24:13 -0500

    quick ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [connected_address, setConnected_address] = useState<string | undefined>(undefined);
+
+    const connect_wallet = () =>{
+        setConnected_address('')
+    }
+
+    return (
+        <div className="App">
+            {connected_address !== undefined ?
+                (
+                    <Home />
+                )
+                :
+                (
+                    <>
+                        <p>Connect Your Wallet</p>
+                        <input type='button' value='Connect' onClick={() => connect_wallet()}/>
+                    </>
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+interface HomeProps {
+
+}
+
+
+
+export const Home: React.FC<HomeProps> = ({ }) => {
+
+    // implement these, props from app.ts?
+    const energy = 100;
+    const currency = 100;
+
+
+    // unimplemented
+    const Deck = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    //unimplemented
+    const buy_deck = () => {
+        console.log('beggining deck purchase');
+    }
+
+    return (
+        <>
+            <p>Home Page</p>
+
+            {Deck.map((card, index) =>
+                <div key={index}>
+                    <p>Card number{card}</p>
+                </div>
+            )}
+
+            <div id='buydeck'>
+                <h1>Buy a deck</h1>
+                <input type='button' value='Buy' onClick={() => buy_deck()}/>
+            </div>
+
+            <div id='liquiditypool'>
+                <h1>Liquidity Pools</h1>
+                <input type='button' value='View Liquidity Pools' onClick={() => console.log('Clicked Liquidity Pools')}/>
+            </div>
+            
+            <div id='lobbies'>
+                <h1>Find A Match</h1>
+                <input type='button' value='Lobbies' onClick={() => console.log('lobbies')} />
+            </div>
+        </>
+    )
+}
