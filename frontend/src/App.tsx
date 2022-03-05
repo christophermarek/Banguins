b96907e60855cd8b7bb40d44ce34ae2e4024986d
christophermarek <christophermarek42@gmail.com>
2022-03-04 22:22:32 -0500

    metamask connection setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import { Home } from './Home';
 import { Marketplace } from './Marketplace';
+import { ethers } from 'ethers';
 
 function App() {
 
-    const [connected_address, setConnected_address] = useState<string | undefined>(undefined);
+    //private key and can sign things
+    const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner | undefined>(undefined);
+    // connection to the Etherum network (READ ONLY)
+    const [provider, setProvider] = useState<ethers.providers.Web3Provider | undefined>(undefined);
+    
     const [pageSelected, setPageSelected] = useState<string>('Home');
 
     // needs to hook to metamask
-    const connect_wallet = () => {
-        setConnected_address('')
+    const connect_wallet = async() => {
+        // A Web3Provider wraps a standard Web3 provider, which is
+        // what MetaMask injects as window.ethereum into each page
+        const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+
+        // MetaMask requires requesting permission to connect users accounts
+        await provider.send("eth_requestAccounts", []);
+
+        // The MetaMask plugin also allows signing transactions to
+        // send ether and pay to change state within the blockchain.
+        // For this, you need the account signer...
+        const signer = provider.getSigner();
+        setSigner(signer);
+        setProvider(provider);
     }
 
     return (
 
         <div className="App">
-
-
-            {connected_address !== undefined ?
+            {provider !== undefined && signer !== undefined ?
                 (
                     <>
                         <div id='navbar' >
@@ -35,8 +50,6 @@ function App() {
                             }
                         </>
                     </>
-
-
                 )
                 :
                 (
