03e4ef72f0763a419d299c674a2a704502e468b1
christophermarek <christophermarek42@gmail.com>
2022-03-06 13:00:44 -0500

    heroku config

--- server/app.ts
+++ server/app.ts
@@ -11,7 +11,8 @@ app.use(cors())
 app.use(routes)
 
 
-app.listen(port, () => {
+// This is for heroku, heroku dynamically assigns port
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on port ${port}.`);
 });
 
