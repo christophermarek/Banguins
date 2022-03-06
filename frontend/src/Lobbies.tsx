8ef6f53b845658126c4cbeb2d12718b2651a453b
christophermarek <christophermarek42@gmail.com>
2022-03-05 20:53:55 -0500

    frontend api to create lobby

--- frontend/src/Lobbies.tsx
+++ frontend/src/Lobbies.tsx
@@ -1,14 +1,20 @@
+import axios, { AxiosResponse } from "axios"
+
 import { useState } from "react";
 import { BattleView } from "./BattleView";
+
+const baseUrl: string = "http://localhost:8000"
 
 interface LobbiesProps {
     setLobbiesSelected: any
 }
 
-
 export const Lobbies: React.FC<LobbiesProps> = ({ setLobbiesSelected }) => {
 
     const [battleView, setBattleView] = useState<boolean>(false);
+
+    const [createLobbySelected, setCreateLobbySelected] = useState<boolean>(false);
+
 
     // unimplemented
     interface lobbies_display {
@@ -27,21 +33,50 @@ export const Lobbies: React.FC<LobbiesProps> = ({ setLobbiesSelected }) => {
         }
     }
 
+    // cards_selected is the array of card address's
+    const createLobby = async (cards_selected: string[]) => {
+        setCreateLobbySelected(false);
+
+        // post server
+        try {
+            const response: AxiosResponse<any> = await axios.post(
+                baseUrl + "/create_lobby",
+                {wallet: '0xwallet', cards: cards_selected}
+            )
+            
+            alert(`Successfully created lobby with id=${response.data.lobby_id}, now wait in lobbies until opponent joined`);
+        } catch (error: any) {
+            alert('Error creating lobby');
+        }
+        // if success then alert client saying successfully created, dont leave screen until opponent joined
+
+    }
+
     return (
         <>
             {battleView ?
                 (
-                    <BattleView setBattleView={setBattleView}/>
+                    <BattleView setBattleView={setBattleView} />
                 )
                 :
                 (
                     <>
+                        {createLobbySelected &&
+                            <div id='createlobby'>
+                                <p>Card1 selected</p>
+                                <p>Card2 selected</p>
+                                <p>Card3 selected</p>
+                                <input type='button' value='Create Lobby' onClick={() => createLobby(
+                                    ['0xasdsadsadasd', '0xasdasdasdsad', '0xzx12213213']
+                                )} />
+                            </div>
+                        }
                         <input type='button' value='Go Back' onClick={() => setLobbiesSelected(false)} />
 
                         <h1>Lobbies</h1>
 
                         {/* unimplemented */}
-                        <input type='button' value='Create Lobby' onClick={() => console.log('createLobby')} />
+                        <input type='button' value='Create Lobby' onClick={() => setCreateLobbySelected(true)} />
 
                         <h1>View Lobbies</h1>
                         <div className='lobby_display'>
@@ -58,8 +93,6 @@ export const Lobbies: React.FC<LobbiesProps> = ({ setLobbiesSelected }) => {
                     </>
                 )
             }
-
-
         </>
     )
 }
